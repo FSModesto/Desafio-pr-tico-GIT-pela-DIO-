acfbfa0065ca95899e76a13dc85d49d5eee752e3
FSModesto <felipesouzausjt@gmail.com>
2022-03-04 14:20:25 -0300

    exercicio de tabuada usando for adicionado

---
+++ Exercicio_for_tabuada/Exercicio_for_tabuada/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercicio_for_tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n;
+            int multiplicação;
+            
+
+            Console.WriteLine("Digite o número para a tabuada:");
+
+            n = int.Parse(Console.ReadLine());
+
+            if (2 < n && n < 100)
+            {
+               
+                for (int i = 0; i < 11; i++)
+                {
+                    
+                    multiplicação = n * i;
+                    Console.WriteLine(i + " x " + n + " = " + multiplicação);
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Impossível calcular");
+            }
+
+
+        }
+    }
+}
